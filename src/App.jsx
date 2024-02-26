951d428fc7a5a36e6258df2c039208050355a5e7
ilhamyon <ilhamyon@gmail.com>
2024-02-26 20:50:57 +0700

    fix: route home

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/kamar-mandi" element={<WC />} />
       </Routes>
     </Router>
